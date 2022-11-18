5b13331163b86ddeecff45971fa2ba77264348ee
Ruiz968 <jose.ruiz.alarcon@gmail.com>
2022-11-18 12:08:25 +0100

    Added tests to Relations

--- tests/test_relations.py
+++ tests/test_relations.py
@@ -120,9 +120,9 @@ def test_add_remove_relations():
     a, b, c = Arg('a'), Arg('b'), Arg('c')
     relations = QBAFARelations([(b,a), (c,a)])
     relations.add(a,b)
-    relations.relations == {(b,a), (c,a), (a,b)}
+    assert relations.relations == {(b,a), (c,a), (a,b)}
     relations.add(a,c)
-    relations.relations == {(b,a), (c,a), (a,b), (a,c)}
+    assert relations.relations == {(b,a), (c,a), (a,b), (a,c)}
     relations.remove(b,a)
     relations.remove(c,a)
     assert relations.relations == {(a,b), (a,c)}
@@ -163,9 +163,139 @@ def test_add_remove_contains():
 
 # TEST COPY
 
-# TODO
+def test_copy_relations():
+    a, b, c = Arg('a'), Arg('b'), Arg('c')
+    relations = QBAFARelations([(a,b), (a,c)])
+    copy = relations.copy()
+    assert copy.relations == {(a,b), (a,c)}
+
+
+def test_copy_patients():
+    a, b, c = Arg('a'), Arg('b'), Arg('c')
+    relations = QBAFARelations([(a,b), (a,c)])
+    copy = relations.copy()
+    assert set(copy.patients(a)) == {b, c}
+    assert set(copy.patients(b)) == set()
+    assert set(copy.patients(c)) == set()
+
+def test_copy_agents():
+    a, b, c = Arg('a'), Arg('b'), Arg('c')
+    relations = QBAFARelations([(a,b), (a,c)])
+    copy = relations.copy()
+    assert set(copy.agents(a)) == set()
+    assert set(copy.agents(b)) == {a}
+    assert set(copy.agents(c)) == {a}
+
+def test_copy_contains():
+    a, b, c = Arg('a'), Arg('b'), Arg('c')
+    relations = QBAFARelations([(a,b), (a,c)])
+    copy = relations.copy()
+    assert copy.contains(a, b)
+    assert copy.contains(a, c)
+    assert not copy.contains(b, a)
+    assert not copy.contains(c, a)
+
+def test_copy_remove_relations():
+    a, b, c = Arg('a'), Arg('b'), Arg('c')
+    relations = QBAFARelations([(a,b), (a,c), (b,a), (c,a)])
+    copy = relations.copy()
+    relations.remove(b,a)
+    assert copy.relations == {(a,b), (a,c), (b,a), (c,a)}
+    assert relations.relations == {(a,b), (a,c), (c,a)}
+    copy.remove(c,a)
+    assert copy.relations == {(a,b), (a,c), (b,a)}
+    assert relations.relations == {(a,b), (a,c), (c,a)}
+    relations.remove(c,a)
+    assert copy.relations == {(a,b), (a,c), (b,a)}
+    assert relations.relations == {(a,b), (a,c)}
+
+def test_copy_remove_patients():
+    a, b, c = Arg('a'), Arg('b'), Arg('c')
+    relations = QBAFARelations([(a,b), (a,c), (b,a), (c,a)])
+    copy = relations.copy()
+    relations.remove(b,a)
+    copy.remove(c,a)
+    assert set(relations.patients(a)) == {b, c}
+    assert set(relations.patients(b)) == set()
+    assert set(relations.patients(c)) == {a}
+    assert set(copy.patients(a)) == {b, c}
+    assert set(copy.patients(b)) == {a}
+    assert set(copy.patients(c)) == set()
+
+def test_copy_remove_agents():
+    a, b, c = Arg('a'), Arg('b'), Arg('c')
+    relations = QBAFARelations([(a,b), (a,c), (b,a), (c,a)])
+    copy = relations.copy()
+    relations.remove(b,a)
+    copy.remove(c,a)
+    assert set(relations.agents(a)) == {c}
+    assert set(relations.agents(b)) == {a}
+    assert set(relations.agents(c)) == {a}
+    assert set(copy.agents(a)) == {b}
+    assert set(copy.agents(b)) == {a}
+    assert set(copy.agents(c)) == {a}
+
+def test_copy_remove_contains():
+    a, b, c = Arg('a'), Arg('b'), Arg('c')
+    relations = QBAFARelations([(a,b), (a,c), (b,a), (c,a)])
+    copy = relations.copy()
+    relations.remove(b,a)
+    relations.remove(c,a)
+    assert relations.contains(a, b) and relations.contains(a, c)
+    assert not relations.contains(b, a) and not relations.contains(c, a)
+    assert copy.contains(a, b) and copy.contains(a, c)
+    assert copy.contains(b, a) and copy.contains(c, a)
+
+def test_copy_add_relations():
+    a, b, c = Arg('a'), Arg('b'), Arg('c')
+    relations = QBAFARelations([(b,a), (c,a)])
+    copy = relations.copy()
+    relations.add(a,b)
+    assert relations.relations == {(b,a), (c,a), (a,b)}
+    assert copy.relations == {(b,a), (c,a)}
+    copy.add(a,c)
+    assert relations.relations == {(b,a), (c,a), (a,b)}
+    assert copy.relations == {(b,a), (c,a), (a,c)}
+
+def test_copy_add_patients():
+    a, b, c = Arg('a'), Arg('b'), Arg('c')
+    relations = QBAFARelations([(a,b), (a,c)])
+    copy = relations.copy()
+    relations.add(b,a)
+    copy.add(c,a)
+    assert set(relations.patients(a)) == {b, c}
+    assert set(relations.patients(b)) == {a}
+    assert set(relations.patients(c)) == set()
+    assert set(copy.patients(a)) == {b, c}
+    assert set(copy.patients(b)) == set()
+    assert set(copy.patients(c)) == {a}
+
+def test_copy_add_agents():
+    a, b, c = Arg('a'), Arg('b'), Arg('c')
+    relations = QBAFARelations([(a,b), (a,c)])
+    copy = relations.copy()
+    relations.add(b,a)
+    copy.add(c,a)
+    assert set(relations.agents(a)) == {b}
+    assert set(relations.agents(b)) == {a}
+    assert set(relations.agents(c)) == {a}
+    assert set(copy.agents(a)) == {c}
+    assert set(copy.agents(b)) == {a}
+    assert set(copy.agents(c)) == {a}
+
+def test_copy_add_contains():
+    a, b, c = Arg('a'), Arg('b'), Arg('c')
+    relations = QBAFARelations([(a,b), (a,c)])
+    copy = relations.copy()
+    relations.add(b,a)
+    copy.add(c,a)
+    assert relations.contains(a, b) and relations.contains(a, c)
+    assert relations.contains(b, a) and not relations.contains(c, a)
+    assert copy.contains(a, b) and copy.contains(a, c)
+    assert not copy.contains(b, a) and copy.contains(c, a)
 
 # TEST LEN
+
 def test_len():
     a, b, c = Arg('a'), Arg('b'), Arg('c')
     relations = QBAFARelations([(a,b), (a,c)])
