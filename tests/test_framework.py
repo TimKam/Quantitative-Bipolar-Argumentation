ac8a2b7bcb7c1d68d059bf29f48eee34c05b7ec3
Ruiz968 <jose.ruiz.alarcon@gmail.com>
2022-11-22 17:49:51 +0100

    Added tests

--- tests/test_framework.py
+++ tests/test_framework.py
@@ -127,6 +127,7 @@ def test_access_attack_relations():
     assert qbf.attack_relations.relations == {('a', 'c')}
     assert qbf.contains_attack_relation('a', 'c')
     assert not qbf.contains_attack_relation('c', 'a')
+    assert not qbf.contains_attack_relation('a', 'b')
     for a, b in [('a', 'c'), ('a', 'b'), ('c', 'a'), ('b', 'a')]:
         assert qbf.contains_attack_relation(a,b) == qbf.attack_relations.contains(a, b)
         assert qbf.contains_attack_relation(a,b) == ((a,b) in qbf.attack_relations)
@@ -137,6 +138,13 @@ def test_modify_attack_relations():
     assert ('a', 'c') not in qbf.attack_relations
     qbf.add_attack_relation('a', 'c')
     assert ('a', 'c') in qbf.attack_relations
+
+    with pytest.raises(ValueError):
+        qbf.add_attack_relation('d', 'c')
+    with pytest.raises(ValueError):
+        qbf.add_attack_relation('c', 'd')
+    qbf.remove_attack_relation('d', 'c')
+    qbf.remove_attack_relation('c', 'd')
     
     with pytest.raises(PermissionError):
         qbf.attack_relations.add('b', 'c')
@@ -145,3 +153,116 @@ def test_modify_attack_relations():
 
 
 # TEST SUPPORT RELATIONS
+
+def test_access_support_relations():
+    qbf = QBAFramework(['a', 'b', 'c'], [1, 1, 5], [('a', 'c')], [('a', 'b')])
+    assert qbf.support_relations.relations == {('a', 'b')}
+    assert qbf.contains_support_relation('a', 'b')
+    assert not qbf.contains_support_relation('b', 'a')
+    assert not qbf.contains_support_relation('a', 'c')
+    for a, b in [('a', 'c'), ('a', 'b'), ('c', 'a'), ('b', 'a')]:
+        assert qbf.contains_support_relation(a,b) == qbf.support_relations.contains(a, b)
+        assert qbf.contains_support_relation(a,b) == ((a,b) in qbf.support_relations)
+
+def test_modify_support_relations():
+    qbf = QBAFramework(['a', 'b', 'c'], [1, 1, 5], [('a', 'c')], [('a', 'b')])
+    qbf.remove_support_relation('a', 'b')
+    assert ('a', 'b') not in qbf.support_relations
+    qbf.add_support_relation('a', 'b')
+    assert ('a', 'b') in qbf.support_relations
+
+    with pytest.raises(ValueError):
+        qbf.add_support_relation('d', 'c')
+    with pytest.raises(ValueError):
+        qbf.add_support_relation('c', 'd')
+    qbf.remove_support_relation('d', 'c')
+    qbf.remove_support_relation('c', 'd')
+    
+    with pytest.raises(PermissionError):
+        qbf.support_relations.add('b', 'c')
+    with pytest.raises(PermissionError):
+        qbf.support_relations.remove('a', 'b')
+
+# TEST FINAL WEIGHTS
+
+def test_access_final_weight():
+    qbf = QBAFramework(['a', 'b', 'c'], [1, 1, 5], [('a', 'c')], [('a', 'b')])
+    for argument, weight in qbf.final_weights.items():
+        assert qbf.final_weight(argument) == weight
+    with pytest.raises(ValueError):
+        qbf.final_weight('d')
+    assert qbf.final_weights == {'a': 1.0, 'c': 4.0, 'b': 2.0}
+
+    qbfe = qbf.copy()
+    assert qbfe.final_weights == {'a': 1.0, 'c': 4.0, 'b': 2.0}
+    qbfe.add_argument('e', initial_weight=3)
+    qbfe.add_attack_relation('e', 'c')
+    assert qbfe.final_weights == {'a': 1.0, 'e': 3.0, 'c': 1.0, 'b': 2.0}
+
+    qbfa = qbfe.copy()
+    assert qbfa.final_weights == {'a': 1.0, 'e': 3.0, 'c': 1.0, 'b': 2.0}
+    qbfa.remove_attack_relation('e', 'c')
+    qbfa.remove_argument('e')
+    qbfa.modify_initial_weight('a', 2)
+    assert qbfa.final_weights == {'a': 2.0, 'c': 3.0, 'b': 3.0}
+
+    qbf_ = qbfa.copy()
+    qbf_.final_weights == {'a': 2.0, 'c': 3.0, 'b': 3.0}
+    qbf_.add_argument('e', 3)
+    qbf_.add_attack_relation('e', 'c')
+    qbf_.add_argument('d', 1)
+    qbf_.add_attack_relation('d', 'a')
+    qbf_.add_support_relation('d', 'e')
+    qbf_.final_weights == {'d': 1.0, 'a': 1.0, 'e': 4.0, 'c': 0.0, 'b': 2.0}
+
+def test_modify_final_weight():
+    qbf = QBAFramework(['a', 'b', 'c'], [1, 1, 5], [('a', 'c')], [('a', 'b')])
+    final_weights = qbf.final_weights
+    final_weights['a'] = 0.0
+    assert qbf.initial_weight('a') == 1.0
+
+    qbf.modify_initial_weight('c', 4)
+    assert qbf.final_weight('c') == 3.0
+
+    qbf.add_argument('a', 0.0)
+    assert qbf.final_weight('a') == 1.0
+
+# TEST COPY
+
+def test_copy():
+    qbf = QBAFramework(['a', 'b', 'c'], [1, 1, 5], [('a', 'c')], [('a', 'b')], disjoint_relations=False)
+    copy = qbf.copy()
+    assert qbf.arguments == copy.arguments
+    assert qbf.initial_weights == copy.initial_weights
+    assert qbf.attack_relations.relations == copy.attack_relations.relations
+    assert qbf.support_relations.relations == copy.support_relations.relations
+    assert qbf.disjoint_relations == copy.disjoint_relations
+    copy.add_argument('d', 3)
+    copy.modify_initial_weight('c', 3)
+    copy.remove_attack_relation('a', 'c')
+    copy.add_support_relation('a', 'c')
+    copy.disjoint_relations = True
+    assert qbf.arguments != copy.arguments
+    assert qbf.initial_weights != copy.initial_weights
+    assert qbf.attack_relations.relations != copy.attack_relations.relations
+    assert qbf.support_relations.relations != copy.support_relations.relations
+    assert qbf.disjoint_relations != copy.disjoint_relations
+
+# TEST ACYCLIC
+
+def test_isacyclic():
+    assert QBAFramework([],[],[],[]).isacyclic()
+    qbf = QBAFramework(['a', 'b', 'c'], [1, 1, 5], [('a', 'c')], [('a', 'b')], disjoint_relations=False)
+    assert qbf.isacyclic()
+    qbf.add_attack_relation('c', 'a')
+    assert not qbf.isacyclic()
+    with pytest.raises(NotImplementedError):
+        qbf.final_weights
+
+# TODO: TEST STRENGTH CONSISTENCY
+
+# TODO: TEST REVERSAL
+
+# TODO: TEST IS EXPLANATION
+
+# TODO: TEST MINIMAL EXPLANATIONS
