dd6fb05cfbe5e2cb434a61e3363e6fc871acf375
Ruiz968 <jose.ruiz.alarcon@gmail.com>
2022-11-22 11:00:26 +0100

    Added some tests

--- tests/test_framework.py
+++ tests/test_framework.py
@@ -1,5 +1,5 @@
 import pytest
-from qbaf import QBAFramework
+from qbaf import QBAFramework, QBAFARelations
 
 def test_init_correct_args():
     args = ['a', 'b', 'c']
@@ -20,6 +20,8 @@ def test_init_no_args():
 
 def test_init_args_initial_weights():
     QBAFramework(['a', 'b'], [1, 1], [], [])
+    with pytest.raises(TypeError):
+        QBAFramework(['a', 'b'], [1, "1"], [], [])
     with pytest.raises(ValueError):
         QBAFramework(['a', 'b'], [1], [], [])
     with pytest.raises(ValueError):
@@ -46,4 +48,27 @@ def test_init_disjoint_relations():
     with pytest.raises(ValueError):
         QBAFramework(['a', 'b'], [1, 1], [('a', 'b')], [('a', 'b')])
 
+def test_init_getters():
+    qbf = QBAFramework(['a', 'b', 'c'], [1, 1, 5], [('a', 'c')], [('a', 'b')], disjoint_relations=True)
+    assert qbf.arguments == {'a', 'b', 'c'}
+    assert qbf.initial_weights == {'a':1.0, 'b':1.0, 'c':5.0}
+    assert qbf.attack_relations.relations == {('a', 'c')}
+    assert qbf.support_relations.relations == {('a', 'b')}
+    assert qbf.disjoint_relations == True
+    assert qbf.final_weights == {'a': 1.0, 'c': 4.0, 'b': 2.0}
+
+def test_init_setters():
+    qbf = QBAFramework(['a', 'b', 'c'], [1, 1, 5], [('a', 'c')], [('a', 'b')])
+    with pytest.raises(AttributeError):
+        qbf.arguments = set()
+    with pytest.raises(AttributeError):
+        qbf.initial_weights = dict()
+    with pytest.raises(AttributeError):
+        qbf.attack_relations = QBAFARelations([])
+    with pytest.raises(AttributeError):
+        qbf.support_relations = QBAFARelations([])
+    with pytest.raises(AttributeError):
+        qbf.disjoin_relations = False
+    with pytest.raises(AttributeError):
+        qbf.final_weights = dict()
 
