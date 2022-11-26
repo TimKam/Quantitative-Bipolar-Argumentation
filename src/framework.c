8218f6ff85da75986cd70b39ae82c09f1f54412c
Ruiz968 <jose.ruiz.alarcon@gmail.com>
2022-11-27 00:01:36 +0100

    Added definition if stricmp does not exist

--- src/framework.c
+++ src/framework.c
@@ -15,6 +15,19 @@
 #include "qbaf_utils.h"
 #include "qbaf_functions.h"
 
+#ifndef stricmp
+#include <ctype.h>
+int stricmp(const char *s1, const char *s2) {
+    while (tolower((unsigned char) *s1) == tolower((unsigned char) *s2)) {
+        if (*s1 == '\0')
+            return 0;
+        s1++; s2++;
+    }
+
+    return (int) tolower((unsigned char) *s1) - (int) tolower((unsigned char) *s2);
+}
+#endif
+
 #define TRUE 1
 #define FALSE 0
 
