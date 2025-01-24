ae0aacffff71eb8b5467bf48d44b366ddcdc0c82
Filip Naudot <filip.naudot@gmail.com>
2025-01-24 16:25:58 +0000

    Modified determine_removal_ctrb to handle set of contributors.

--- qbaf_ctrbs/removal.py
+++ qbaf_ctrbs/removal.py
@@ -1,23 +1,27 @@
 from qbaf_ctrbs.utils import restrict
 
-def determine_removal_ctrb(topic, contributor, qbaf):
+def determine_removal_ctrb(topic, contributors, qbaf):
     """Determines the removal contribution of a contributor
     to a topic argument.
 
     Args:
         topic (string): The topic argument
-        contributor (string): The contributing argument
+        contributor (string | set): The contributing argument(s)
         qbaf (QBAFramework): The QBAF that contains topic and contributor
 
     Returns:
         float: The contribution of the contributor to the topic
     """
-    if topic == contributor:
+    if topic in contributors:
         raise Exception (
             'An argument\'s removal contribution to itself cannot be determined.')
-    if not topic in qbaf.arguments or not contributor in qbaf.arguments:
-        raise Exception ('Topic and contributor must be in the QBAF.')
+    # Ensure topic and argument(s) are in QBAF.
+    if not all(item in qbaf.arguments for item in [topic, *contributors]):
+            raise Exception ('Topic and contributor must be in the QBAF.')
+    # Check if input is a set of arguments (type: set) or single argument (type: string).
+    if not isinstance(contributors, set):
+        contributors = {contributors}
     fs_with = qbaf.final_strengths[topic]
-    restriction = restrict(qbaf, qbaf.arguments - {contributor})
+    restriction = restrict(qbaf, qbaf.arguments - contributors)
     fs_without = restriction.final_strengths[topic]
-    return fs_with - fs_without 
+    return  fs_with - fs_without
