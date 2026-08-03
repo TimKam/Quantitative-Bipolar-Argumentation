b5f11d2e341976828c2cc0826f5f2e99de86f877
PseudoCoder_Arun <arunavo.rbd@gmail.com>
2026-08-03 14:52:17 +0200

    Added notions of safety, oscillations, liveness and fairness.

---
+++ qbaf_solf/fairness_notions.py
@@ -0,0 +1,83 @@
+from qbaf import QBAFramework
+from qbaf_solf.safety_liveness_oscillations import *
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+
+
+def calculate_gini_fairness(qbaf_collection: list[QBAFramework],
+                            topic_set: list[str],
+                            threshold: float) -> list:
+
+    """
+      Calculates the Gini oscillation fainress of the topic set.
+
+      Args:
+        qbaf_collection (list[QBAFramework]): The collection of QBAFs.
+        topic_set (list[str]): The considered set of arguments.
+        threshold (float): The justification threshold.
+      Returns:
+        float: returns the Gini oscillation fairness score.
+
+    """
+
+    x_axis = [x for x in range(0, len(topic_set)+1)]
+
+    # Calculating the safety curve
+    sorted_oscillations = sorted(number_of_oscillations(qbaf_collection, topic_set, threshold).items(), key=lambda item: item[1])
+    safety_curve = [0]
+    for x in sorted_oscillations:
+      safety_curve.append(safety_curve[-1] + int(x[1]))
+
+    # Calculating the fairness line
+    fairness_line = np.linspace(0, safety_curve[-1], len(topic_set)+1)
+
+    # Calulating Gini fairness
+    area_enclosed = np.trapezoid(np.abs(fairness_line - safety_curve), x_axis)
+    gini_fairness = (2 / (1 + math.e ** (-area_enclosed))) - 1
+
+    # Plotting the safety curve and fairness line
+    plt.xticks(x_axis, ['0']+[x[0] for x in sorted_oscillations])
+    plt.plot(x_axis, safety_curve, label = 'Safety Curve', color='green', marker = 's')
+    plt.plot(x_axis, fairness_line, label = 'Fairness Line', linestyle = 'dashed', color='red')
+    plt.fill_between(x_axis, safety_curve, fairness_line, alpha=0.4)
+    plt.legend()
+    plt.show()
+
+    return gini_fairness
+
+
+def calculate_shannon_fairness(qbaf_collection: list[QBAFramework],
+                               topic_set: list[str],
+                               threshold: float) -> list:
+
+    """
+      Calculates the Shannon oscillation fainress of the topic set.
+
+      Args:
+        qbaf_collection (list[QBAFramework]): The collection of QBAFs.
+        topic_set (list[str]): The considered set of arguments.
+        threshold (float): The justification threshold.
+      Returns:
+        float: returns the Shannon oscillation fairness score.
+
+    """
+
+    x_axis = [x for x in range(0, len(topic_set)+1)]
+
+
+    # Calculating the oscillation probability
+    oscillations = number_of_oscillations(qbaf_collection, topic_set, threshold)
+    if all(oscillations[x] == 0 for x in oscillations.keys()):
+      return 1
+    sum_of_oscillations = sum([x[1] for x in oscillations.items()])
+    oscillation_probability = {x: oscillations[x]/sum_of_oscillations for x in oscillations.keys()}
+    print(oscillation_probability)
+
+
+    # Calculating the information coefficient
+    info_coefficient = {x: (-math.log(oscillation_probability[x])/math.log(len(topic_set))) for x in oscillations.keys() if oscillation_probability[x] > 0}
+    shannon_fairness = sum([oscillation_probability[x] * info_coefficient[x] for x in info_coefficient.keys()])
+
+
+    return shannon_fairness
---
+++ qbaf_solf/safety_oscillations_liveness.py
@@ -0,0 +1,77 @@
+from qbaf import QBAFramework
+
+def is_safe(qbaf_collection: list[QBAFramework],
+            topic_set: list[str],
+            threshold: float) -> bool:
+    """
+      Returns whether a topic set is safe w.r.t. qbaf_collection, and treshold.
+
+      Args:
+        qbaf_collection (list[QBAFramework]): The list QBAFs w.r.t. which safety is measured.
+        topic_set (list[str]): The set of topic arguments for which safety is measured.
+        threshold (float): The threshold of the topic set.
+
+      Returns:
+        bool: true if the topic_set is safe, false otherwise.
+
+    """
+
+    for qbaf in qbaf_collection:
+      for arg in topic_set:
+        if (qbaf.final_strength(arg)<threshold):
+          return False
+
+    return True
+
+
+def is_live(qbaf_collection: list[QBAFramework],
+            topic_set: list[str],
+            threshold: float) -> bool:
+    """
+      Returns whether a topic set is live w.r.t. qbaf_collection, and treshold.
+
+      Args:
+        qbaf_collection (list[QBAFramework]): The list QBAFs w.r.t. which liveness is measured.
+        topic_set (list[str]): The set of topic arguments for which liveness is measured.
+        threshold (float): The threshold of the topic set.
+
+      Returns:
+        bool: true if the topic_set is live, false otherwise.
+
+    """
+
+    qbaf = qbaf_collection[len(qbaf_collection)-1]
+    for arg in topic_set:
+      if (qbaf.final_strength(arg)<threshold):
+        return False
+
+    return True
+
+
+def number_of_oscillations(qbaf_collection: list[QBAFramework],
+                           topic_set: list[str],
+                           threshold: float) -> dict:
+
+    """
+      Returns the number of oscillations of a topic set w.r.t. qbaf_collection, and treshold.
+
+      Args:
+        qbaf_collection (list[QBAFramework]): The list QBAFs.
+        topic_set (list[str]): The set of topic arguments for which the oscillations are measured.
+        threshold (float): The threshold of the topic set.
+
+      Returns:
+        dict: the number of oscillations of the topic_set.
+
+    """
+    oscillations = {x: 0 for x in topic_set}
+
+    for i in range(0, len(qbaf_collection)-1):
+      for x in topic_set:
+        if (qbaf_collection[i].final_strength(x)< threshold and qbaf_collection[i+1].final_strength(x) >= threshold):
+          oscillations[x] += 1
+        elif (qbaf_collection[i].final_strength(x)>= threshold and qbaf_collection[i+1].final_strength(x) < threshold):
+          oscillations[x] += 1
+
+
+    return oscillations
